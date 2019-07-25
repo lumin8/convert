0b7cf2afe85d56b11054b1a3e4fa044b32546786
Scott Close <scott@ethosgeo.com>
2019-07-25 21:51:03 +0000

    fixed bug where dataset was blank

--- convert.go
+++ convert.go
@@ -1,353 +1,368 @@
-	package convert
+package convert
 
-	import (
-		"encoding/csv"
-		"errors"
-		"fmt"
-		"io"
-		"io/ioutil"
-		"math"
-		"os"
-		"path"
-		"path/filepath"
-		"strconv"
-		"sync"
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"sync"
 
-		"github.com/golang/geo/s2"
-		"github.com/lumin8/elev-utils"
-		"github.com/paulmach/go.geo"
-		"github.com/paulmach/go.geojson"
-		"github.com/remeh/sizedwaitgroup"
-	)
+	"github.com/golang/geo/s2"
+	"github.com/lumin8/elev-utils"
+	"github.com/paulmach/go.geo"
+	"github.com/paulmach/go.geojson"
+	"github.com/remeh/sizedwaitgroup"
+)
 
-	// Datasets ...
-	type Datasets struct {
-		ID      string       `json:"id" yaml:"id"`
-		Name    string       `json:"name" yaml:"name"`
-		Url     string       `json:"dataurl" yaml:"dataurl"`
-		Updated string       `json:"lastUpdated" yaml:"lastUpdated"`
-		Center  []Point      `json:"center" yaml:"center"`
-		S2      []string     `json:"s2" yaml:"s2"`
-		Points  []Points     `json:"points" yaml:"points"`
-		Lines   []Lines      `json:"lines" yaml:"lines"`
-		Shapes  []Shapes     `json:"shapes" yaml:"shapes"`
+// Datasets ...
+type Datasets struct {
+	ID      string       `json:"id" yaml:"id"`
+	Name    string       `json:"name" yaml:"name"`
+	Url     string       `json:"dataurl" yaml:"dataurl"`
+	Updated string       `json:"lastUpdated" yaml:"lastUpdated"`
+	Center  []Point      `json:"center" yaml:"center"`
+	S2      []string     `json:"s2" yaml:"s2"`
+	Points  []Points     `json:"points" yaml:"points"`
+	Lines   []Lines      `json:"lines" yaml:"lines"`
+	Shapes  []Shapes     `json:"shapes" yaml:"shapes"`
+}
+
+// Individual Point Coordinate ...
+type Point struct {
+	X float64 `json:"x" yaml:"x"`
+	Y float64 `json:"y" yaml:"y"`
+	Z float64 `json:"z" yaml:"z"`
+}
+
+// Points ...
+type Points struct {
+	ID         string      `json:"id" yaml:"id"`
+	Name       string      `json:"name" yaml:"name"`
+	StyleType  string      `json:"type" yaml:"type"`
+	Attributes []Attribute `json:"attributes" yaml:"attributes"`
+	Points     []float64   `json:"point" yaml:"point"`
+}
+
+// PointArrays ...
+type PointArray struct {
+	Points     [][]float64 `json:"points" yaml:"points"`
+}
+
+// Line ...
+type Lines struct {
+	ID         string      `json:"id" yaml:"id"`
+	Name       string      `json:"name" yaml:"name"`
+	StyleType  string      `json:"type" yaml:"type"`
+	Attributes []Attribute `json:"attributes" yaml:"attributes"`
+	Points     [][]float64 `json:"points" yaml:"points"`
+}
+
+// Shape ...
+type Shapes struct {
+	ID         string      `json:"id" yaml:"id"`
+	Name       string      `json:"name" yaml:"name"`
+	StyleType  string      `json:"type" yaml:"type"`
+	Attributes []Attribute `json:"attributes" yaml:"attributes"`
+	Points     [][]float64 `json:"points" yaml:"points"`
+}
+
+// Attribute ...
+type Attribute struct {
+	Key   string `json:"key" yaml:"key"`
+	Value string `json:"value" yaml:"value"`
+}
+
+// FeatureInfo ...
+type FeatureInfo struct {
+	ID        string
+	Geojson   geojson.Feature
+	GeomType  string
+	SRID      string
+	S2        []s2.CellID
+	Tokens    []string
+	Name      string
+	StyleType string
+}
+
+// BBOX ExtentContainer
+type ExtentContainer struct {
+	bbox	map[string]float64
+	ch	chan []float64
+	wg	sizedwaitgroup.SizedWaitGroup
+}
+
+const (
+	// env var for the dem.ver path
+	envDEMVRT = "DEMVRT"
+
+	// process limits for the sized wait group
+	maxRoutines = 50
+)
+
+// demvrt is used to cache the path of the dem.vrt file after it has been resolved once.
+// Note: if the file is moved or deleted the path will not change
+var demvrt = ""
+
+func init () {
+	demloc, err := demvrtPath()
+	if err != nil {
+		fmt.Printf("%s",err.Error())
+		return
+	}
+	fmt.Printf("***Initialization Complete: DEM found at: %s***\n",demloc)
+}
+
+
+// demvrtPath is used to resolve the path for the dem.vrt file
+func demvrtPath() (string, error) {
+	if demvrt != "" {
+		return demvrt, nil
 	}
 
-	// Individual Point Coordinate ...
-	type Point struct {
-		X float64 `json:"x" yaml:"x"`
-		Y float64 `json:"y" yaml:"y"`
-		Z float64 `json:"z" yaml:"z"`
-	}
-
-	// Points ...
-	type Points struct {
-		ID         string      `json:"id" yaml:"id"`
-		Name       string      `json:"name" yaml:"name"`
-		StyleType  string      `json:"type" yaml:"type"`
-		Attributes []Attribute `json:"attributes" yaml:"attributes"`
-		Points     []float64   `json:"point" yaml:"point"`
-	}
-
-	// PointArrays ...
-	type PointArray struct {
-		Points     [][]float64 `json:"points" yaml:"points"`
-	}
-
-	// Line ...
-	type Lines struct {
-		ID         string      `json:"id" yaml:"id"`
-		Name       string      `json:"name" yaml:"name"`
-		StyleType  string      `json:"type" yaml:"type"`
-		Attributes []Attribute `json:"attributes" yaml:"attributes"`
-		Points     [][]float64 `json:"points" yaml:"points"`
-	}
-
-	// Shape ...
-	type Shapes struct {
-		ID         string      `json:"id" yaml:"id"`
-		Name       string      `json:"name" yaml:"name"`
-		StyleType  string      `json:"type" yaml:"type"`
-		Attributes []Attribute `json:"attributes" yaml:"attributes"`
-		Points     [][]float64 `json:"points" yaml:"points"`
-	}
-
-	// Attribute ...
-	type Attribute struct {
-		Key   string `json:"key" yaml:"key"`
-		Value string `json:"value" yaml:"value"`
-	}
-
-	// FeatureInfo ...
-	type FeatureInfo struct {
-		ID        string
-		Geojson   geojson.Feature
-		GeomType  string
-		SRID      string
-		S2        []s2.CellID
-		Tokens    []string
-		Name      string
-		StyleType string
-	}
-
-	// BBOX ExtentContainer
-	type ExtentContainer struct {
-		bbox	map[string]float64
-		ch	chan []float64
-		wg	sizedwaitgroup.SizedWaitGroup
-	}
-
-	const (
-		// env var for the dem.ver path
-		envDEMVRT = "DEMVRT"
-
-		// process limits for the sized wait group
-		maxRoutines = 50
-	)
-
-	// demvrt is used to cache the path of the dem.vrt file after it has been resolved once.
-	// Note: if the file is moved or deleted the path will not change
-	var demvrt = ""
-
-	func init () {
-		demloc, err := demvrtPath()
+	dvp := os.Getenv(envDEMVRT)
+	if len(dvp) == 0 {
+		cwd, err := os.Getwd()
 		if err != nil {
-			fmt.Printf("%s",err.Error())
-			return
+			return "", err
 		}
-		fmt.Printf("***Initialization Complete: DEM found at: %s***\n",demloc)
+		dvp = path.Join(cwd, "earthdem.vrt")
 	}
 
+	if _, err := os.Stat(dvp); err != nil {
+		return "", fmt.Errorf("error: world digital elevation model (DEM) cannot be found at %s", demvrt)
+	}
+	demvrt = dvp
 
-	// demvrtPath is used to resolve the path for the dem.vrt file
-	func demvrtPath() (string, error) {
-		if demvrt != "" {
-			return demvrt, nil
-		}
+	return dvp, nil
+}
 
-		dvp := os.Getenv(envDEMVRT)
-		if len(dvp) == 0 {
-			cwd, err := os.Getwd()
-			if err != nil {
-				return "", err
-			}
-			dvp = path.Join(cwd, "earthdem.vrt")
-		}
+// DatasetFromCSV ...
+func DatasetFromCSV(xField string, yField string, zField string, contents io.Reader) (*Datasets, error) {
+	var outdataset Datasets
 
-		if _, err := os.Stat(dvp); err != nil {
-			return "", fmt.Errorf("error: world digital elevation model (DEM) cannot be found at %s", demvrt)
-		}
-		demvrt = dvp
-
-		return dvp, nil
+	raw, err := csv.NewReader(contents).ReadAll()
+	if err != nil {
+		return &outdataset, err
 	}
 
-	// DatasetFromCSV ...
-	func DatasetFromCSV(xField string, yField string, zField string, contents io.Reader) (*Datasets, error) {
-		raw, err := csv.NewReader(contents).ReadAll()
-		if err != nil {
-			return nil, err
-		}
+	if len(raw) == 0 {
+		err = errors.New("no data in dataset")
+		return &outdataset, err
+	}
 
-		var outdataset Datasets
+	//store the csv headers by index
+	headers := make(map[int]string)
+	container := initExtentContainer()
 
-		//store the csv headers by index
-		headers := make(map[int]string)
-		container := initExtentContainer()
-
-		for i, record := range raw {
-			switch i {
-			case 0:
-				for i, header := range record {
-					switch header {
-					case xField:
-						headers[i] = "X"
-					case yField:
-						headers[i] = "Y"
-					case zField:
-						headers[i] = "Z"
-					default:
-						headers[i] = header
-					}
+	for i, record := range raw {
+		switch i {
+		case 0:
+			for i, header := range record {
+				switch header {
+				case xField:
+					headers[i] = "X"
+				case yField:
+					headers[i] = "Y"
+				case zField:
+					headers[i] = "Z"
+				default:
+					headers[i] = header
 				}
-			default:
-				container.wg.Add()
-				go ParseCSV(headers, record, &outdataset, container)
 			}
+		default:
+			container.wg.Add()
+			go ParseCSV(headers, record, &outdataset, container)
 		}
-
-		container.wg.Wait()
-
-		// close the BBOXlistener goroutine
-		close(container.ch)
-
-		// configure the center point... in 4326
-		c := getCenter(container.bbox)
-		outdataset.Center = append(outdataset.Center, c)
-
-		// configure the s2 array... in 4326
-		outdataset.S2 = s2covering(container.bbox)
-
-		return &outdataset, nil
 	}
 
-	// DatasetFromGEOJSON ...
-	func DatasetFromGEOJSON(xField string, yField string, zField string, contents io.Reader) (*Datasets, error) {
-		raw, err := ioutil.ReadAll(contents)
-		if err != nil {
-			return nil, err
-		}
+	container.wg.Wait()
 
-		//carries references to this dataset's ch, wg, and bbox
-		container := initExtentContainer()
+	// close the BBOXlistener goroutine
+	close(container.ch)
 
-		rawjson, err := geojson.UnmarshalFeatureCollection(raw)
+	// configure the center point... in 4326
+	c := getCenter(container.bbox)
+	outdataset.Center = append(outdataset.Center, c)
 
-		// this kicks off the processing of the data
-		outdataset, err := parseGEOJSONCollection(rawjson, container)
-		if err != nil {
-			return outdataset, err
-		}
+	// configure the s2 array... in 4326
+	outdataset.S2 = s2covering(container.bbox)
 
-		// close the BBOXlistener goroutine
-		close(container.ch)
+	return &outdataset, nil
+}
 
-		// configure the center point... in 4326
-		c := getCenter(container.bbox)
-		outdataset.Center = append(outdataset.Center, c)
+// DatasetFromGEOJSON ...
+func DatasetFromGEOJSON(xField string, yField string, zField string, contents io.Reader) (*Datasets, error) {
+	var outdataset *Datasets
 
-		// configure the s2 array... in 4326
-		outdataset.S2 = s2covering(container.bbox)
-
-		return outdataset, nil
+	raw, err := ioutil.ReadAll(contents)
+	if err != nil {
+		return outdataset, err
 	}
 
-	// ParseCSV ...
-	func ParseCSV(headers map[int]string, record []string, outdataset *Datasets, container *ExtentContainer) {
+	if len(raw) == 0 {
+		err = errors.New("no data in dataset")
+		return outdataset, err
+	}
 
-		defer container.wg.Done()
+	//carries references to this dataset's ch, wg, and bbox
+	container := initExtentContainer()
 
-		var xyz []float64
-		var point Points
+	rawjson, err := geojson.UnmarshalFeatureCollection(raw)
+	if err != nil {
+                return outdataset, err
+        }
 
-		for i, value := range record {
-			switch headers[i] {
-			case "X":
-				x, _ := strconv.ParseFloat(value, 64)
-				xyz = append(xyz, x)
-			case "Y":
-				y, _ := strconv.ParseFloat(value, 64)
-				xyz = append(xyz, y)
-			case "Z":
-				z, _ := strconv.ParseFloat(value, 64)
-				xyz = append(xyz, z)
-			default:
-				var atts Attribute
-				atts.Key = headers[i]
-				atts.Value = fmt.Sprintf("%v", value)
-				point.Attributes = append(point.Attributes, atts)
-			}
+	// this kicks off the processing of the data
+	outdataset, err = parseGEOJSONCollection(rawjson, container)
+	if err != nil {
+		return outdataset, err
+	}
+
+	// close the BBOXlistener goroutine
+	close(container.ch)
+
+	// configure the center point... in 4326
+	c := getCenter(container.bbox)
+	outdataset.Center = append(outdataset.Center, c)
+
+	// configure the s2 array... in 4326
+	outdataset.S2 = s2covering(container.bbox)
+
+	return outdataset, nil
+}
+
+// ParseCSV ...
+func ParseCSV(headers map[int]string, record []string, outdataset *Datasets, container *ExtentContainer) {
+
+	defer container.wg.Done()
+
+	var xyz []float64
+	var point Points
+
+	for i, value := range record {
+		switch headers[i] {
+		case "X":
+			x, _ := strconv.ParseFloat(value, 64)
+			xyz = append(xyz, x)
+		case "Y":
+			y, _ := strconv.ParseFloat(value, 64)
+			xyz = append(xyz, y)
+		case "Z":
+			z, _ := strconv.ParseFloat(value, 64)
+			xyz = append(xyz, z)
+		default:
+			var atts Attribute
+			atts.Key = headers[i]
+			atts.Value = fmt.Sprintf("%v", value)
+			point.Attributes = append(point.Attributes, atts)
 		}
+	}
 
-		// enforce 3857 and elevation
-		coord, err := CheckCoords(xyz)
-		if err != nil {
-			// skip a bunk coordinate
+	// enforce 3857 and elevation
+	coord, err := CheckCoords(xyz)
+	if err != nil {
+		// skip a bunk coordinate
+		return
+	}
+
+	// keep a collective of the min / max coords of dataset
+	container.ch <- coord
+
+	// fill in the poiiint float array
+	point.Points = append(point.Points, coord[0], coord[1], coord[2])
+
+	// finally, append point to the final dataset
+	outdataset.Points = append(outdataset.Points, point)
+}
+
+// BBOXListener ...  observes every X & Y on the channel, retains lowest and highest for bbox extent
+func BBOXListener(container *ExtentContainer) {
+
+	for {
+		xyz, ok := <-container.ch
+
+		// if channel closes, kill goroutine
+		if !ok {
 			return
 		}
 
-		// keep a collective of the min / max coords of dataset
-		container.ch <- coord
+		X := xyz[0]
+		Y := xyz[1]
 
-		// fill in the poiiint float array
-		point.Points = append(point.Points, coord[0], coord[1], coord[2])
-
-		// finally, append point to the final dataset
-		outdataset.Points = append(outdataset.Points, point)
-	}
-
-	// BBOXListener ...  observes every X & Y on the channel, retains lowest and highest for bbox extent
-	func BBOXListener(container *ExtentContainer) {
-
-		for {
-			xyz, ok := <-container.ch
-
-			// if channel closes, kill goroutine
-			if !ok {
-				return
-			}
-
-			X := xyz[0]
-			Y := xyz[1]
-
-			_, present := container.bbox["lx"]
-			if !present {
-				container.bbox["lx"] = X
-				container.bbox["rx"] = X
-				container.bbox["ly"] = Y
-				container.bbox["uy"] = Y
-			}
-
-			// if the inbound X is outside of current extent, grow extent
-			if X < container.bbox["lx"] {
-				container.bbox["lx"] = X
-			} else if X > container.bbox["rx"] {
-				container.bbox["ux"] = X
-			}
-
-			// if the inbound Y is outside of current extent, grow extent
-			if Y < container.bbox["ly"] {
-				container.bbox["ly"] = Y
-			} else if Y > container.bbox["uy"] {
-				container.bbox["uy"] = Y
-			}
-		}
-	}
-
-	// getCenter calculates the center of a bbox extent
-	func getCenter(bbox map[string]float64) Point {
-		var c Point
-		c.X = bbox["rx"] - (bbox["rx"]-bbox["lx"])/2
-		c.Y = bbox["uy"] - (bbox["uy"]-bbox["ly"])/2
-		c.Z, _ = GetElev(c.X, c.Y)
-
-		return c
-	}
-
-	// s2covering finds the s2 hash key that represents the geographic coverage of the bbox extent
-	func s2covering(bbox map[string]float64) []string {
-		var s2hash []string
-
-		rx, uy := To4326(bbox["rx"], bbox["uy"])
-		lx, ly := To4326(bbox["lx"], bbox["ly"])
-		cz, err := GetElev(bbox["rx"], bbox["uy"])
-		if err != nil {
-			fmt.Println(err)
+		_, present := container.bbox["lx"]
+		if !present {
+			container.bbox["lx"] = X
+			container.bbox["rx"] = X
+			container.bbox["ly"] = Y
+			container.bbox["uy"] = Y
 		}
 
-		pts := []s2.Point{
-			s2.PointFromCoords(rx, uy, cz),
-			s2.PointFromCoords(lx, uy, cz),
-			s2.PointFromCoords(lx, ly, cz),
-			s2.PointFromCoords(rx, ly, cz),
+		// if the inbound X is outside of current extent, grow extent
+		if X < container.bbox["lx"] {
+			container.bbox["lx"] = X
+		} else if X > container.bbox["rx"] {
+			container.bbox["ux"] = X
 		}
 
-		loop := s2.LoopFromPoints(pts)
-		covering := loop.CellUnionBound()
-
-		for _, cellid := range covering {
-			token := cellid.ToToken()
-			if len(token) > 8 {
-				runes := []rune(token)
-				token = string(runes[0:8])
-			}
-
-			// write s2 token array to the dataset s2 key list
-			s2hash = append(s2hash, token)
+		// if the inbound Y is outside of current extent, grow extent
+		if Y < container.bbox["ly"] {
+			container.bbox["ly"] = Y
+		} else if Y > container.bbox["uy"] {
+			container.bbox["uy"] = Y
 		}
-
-		return s2hash
 	}
+}
+
+// getCenter calculates the center of a bbox extent
+func getCenter(bbox map[string]float64) Point {
+	var c Point
+	c.X = bbox["rx"] - (bbox["rx"]-bbox["lx"])/2
+	c.Y = bbox["uy"] - (bbox["uy"]-bbox["ly"])/2
+	c.Z, _ = GetElev(c.X, c.Y)
+
+	return c
+}
+
+// s2covering finds the s2 hash key that represents the geographic coverage of the bbox extent
+func s2covering(bbox map[string]float64) []string {
+	var s2hash []string
+
+	rx, uy := To4326(bbox["rx"], bbox["uy"])
+	lx, ly := To4326(bbox["lx"], bbox["ly"])
+	cz, err := GetElev(bbox["rx"], bbox["uy"])
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	pts := []s2.Point{
+		s2.PointFromCoords(rx, uy, cz),
+		s2.PointFromCoords(lx, uy, cz),
+		s2.PointFromCoords(lx, ly, cz),
+		s2.PointFromCoords(rx, ly, cz),
+	}
+
+	loop := s2.LoopFromPoints(pts)
+	covering := loop.CellUnionBound()
+
+	for _, cellid := range covering {
+		token := cellid.ToToken()
+		if len(token) > 8 {
+			runes := []rune(token)
+			token = string(runes[0:8])
+		}
+
+		// write s2 token array to the dataset s2 key list
+		s2hash = append(s2hash, token)
+	}
+
+	return s2hash
+}
 
 // GetElev gets the elevation for the given x y coordinate
 func GetElev(x float64, y float64) (float64, error) {
@@ -359,13 +374,15 @@ func GetElev(x float64, y float64) (float64, error) {
 
 	// call the elevation service
         z, err := srtm.ElevationFromLatLon(demdir,lat,lon)
+
+	// uh oh, could not locate the dem lookup
         if err != nil {
                 return z, err
         }
 
 	// raise an error if z not found
 	if math.IsNaN(z) == true {
-		err = errors.New("Z value is NaN")
+		err = errors.New("Z value is NaN, not sure why")
 		return z, err
 	}
 
